174b53a7200e7f09f1246fbe84de9656b3aa1735
Christian Clapton Edison Orioste <clapton.five@gmail.com>
2021-05-24 14:15:30 +0800

    update

--- enshop/templates/pages/utils/home.js
+++ enshop/templates/pages/utils/home.js
@@ -1,6 +1,42 @@
+let perPage = 5;
+
+(function() {
+	window.onresize = displayWindowSize;
+	window.onload = displayWindowSize;
+  
+	function displayWindowSize() {
+	  let myWidth = window.innerWidth;
+	  console.log(myWidth)
+	  // your size calculation code here
+		if(myWidth < 791){
+			perPage =1;
+			new Splide( '#splide', {
+				type   : 'loop',
+				perPage: perPage,
+				perMove: 1,
+				pagination: false,
+				autoplay: true
+			} ).mount();
+		}else{
+			perPage = 5
+			new Splide( '#splide', {
+				type   : 'loop',
+				perPage: perPage,
+				perMove: 1,
+				pagination: false,
+				autoWidth: true,
+				autoplay: true
+			} ).mount();
+		}
+	};
+})()
+
 new Splide( '#splide', {
 	type   : 'loop',
-	perPage: 5,
+	perPage: perPage,
 	perMove: 1,
-    pagination: false,
+	pagination: false,
+	autoplay: true
 } ).mount();
+  
+  
